d1b5a54eb894115b48be5ea880317daa64410bb8
Hoang Nguyen <hnguyen31@gmail.com>
2018-07-29 05:25:57 -0400

    css for sign in page

--- src/components/authentication/SignInScreen.js
+++ src/components/authentication/SignInScreen.js
@@ -5,7 +5,8 @@ import {
     Text,
     View,
     Button,
-    TextInput
+    TextInput,
+    TouchableOpacity
   } from 'react-native';
   import { CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
 
@@ -22,21 +23,49 @@ export default class SignInScreen extends React.Component {
       return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <TextInput
+          style={styles.input}
           placeholder="email"
           // onChange={() => this.setState({email})}
           />
           <TextInput
+          style={styles.input}
           placeholder="password"
           />
-          <Button
+          <TouchableOpacity style={styles.button}>
+            <Text>Log in</Text>
+          </TouchableOpacity>
+          {/* <Button
+          style={styles.button}
           title="Log in"
-          />
-          <Button
+          /> */}
+          {/* <Button
           title="Sign up instead"
           onPress={() => this.props.navigation.navigate('SignUp')}
-          />
+          /> */}
         </View>
       )
       
     }
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+    },
+    input: {
+      width: 300,
+      height: 44,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'black',
+      marginBottom: 10,
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10
+      },
+  });
